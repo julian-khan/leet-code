ee19b17df737aacf42b7c6d3a8b07f025dc98e83
Julian Khan <juliankhan.b@gmail.com>
2023-06-04 19:59:59 +0800

    Complete Three Sum challenge

---
+++ two-pointers/three-sum.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/3sum/
+
+class Solution:
+    
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                if nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                elif nums[i] + nums[left] + nums[right] > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while right > left and nums[right] == nums[right + 1]:
+                        right -= 1
+                
+        return res
