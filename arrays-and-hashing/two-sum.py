1dcd4d492b195153efe465777e7d91eac691d836
Julian Khan <juliankhan.b@gmail.com>
2023-06-05 15:21:06 +0800

    Add linear time complexity solution for Two Sum

--- arrays-and-hashing/two-sum.py
+++ arrays-and-hashing/two-sum.py
@@ -1,6 +1,19 @@
 # https://leetcode.com/problems/two-sum/
 
-# Version 1 with map
+#Version 1 with seen map - O(n) time complexity
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i in range(len(nums)):
+            req = target - nums[i]
+            if req in seen:
+                return [i, seen[req]]
+            else:
+                seen[nums[i]] = i
+
+
+# Version 2 with map
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = { target - nums[i] : i for i in range(len(nums))}
@@ -10,7 +23,7 @@ class Solution:
                 return [i, map[nums[i]]]
 
 
-#Version 2 with nested for loops
+#Version 3 with nested for loops
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
