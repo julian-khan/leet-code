d05355f394b6703bd459a5287410607da7f9b021
Julian Khan <juliankhan.b@gmail.com>
2023-06-07 18:27:12 +0800

    Optimise Valid Parantheses challenge code

--- stack/valid-parantheses.py
+++ stack/valid-parantheses.py
@@ -1,11 +1,18 @@
 # https://leetcode.com/problems/valid-parentheses/
 
+#A more optimised solution than my original version for both run time and memory usage
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        charMap = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+            }
 
         for char in s:
-            if char == '(' or char == '{' or char == '[':
+            if char in charMap:
                 stack.append(char)
                 continue
             elif char == ')':
@@ -19,4 +26,4 @@ class Solution:
                     return False
             stack.pop()
         
-        return True if not stack else False
+        return not stack
