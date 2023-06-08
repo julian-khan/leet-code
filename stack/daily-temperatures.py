cabd596e738e7b48b28e6912359e38587c397d51
Julian Khan <juliankhan.b@gmail.com>
2023-06-08 16:48:03 +0800

    Complete Daily Temperatures challenge

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -5,18 +5,13 @@ class Solution:
         res = [0] * len(temperatures)
         stack = []
 
-        stack.append(temperatures[0])
+        stack.append((temperatures[0], 0))
 
         for i in range(1, len(temperatures)):
             while stack and temperatures[i] > stack[-1][0]:
                 targetIndex = stack.pop()[1]
                 res[targetIndex] = i - targetIndex
             
-            stack.append(tuple(temperatures[i], i))
+            stack.append((temperatures[i], i))
 
         return res
-
-sol = Solution()
-
-temperatures = [73,74,75,71,69,72,76,73]
-Solution.dailyTemperatures(self, temperatures)
