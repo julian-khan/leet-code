a7db56ff8b7987fb6d35ac0388460c6544abab1a
Julian Khan <juliankhan.b@gmail.com>
2023-06-12 11:32:53 +0800

    Complete Reverse Linked List challenge

--- binary-search/median-of-two-sorted-arrays.py
+++ binary-search/median-of-two-sorted-arrays.py
@@ -28,3 +28,36 @@ class Solution:
                 r = mid - 1
             else:
                 l = mid + 1
+
+#An alteration that reduces the complexity of the code by making a recursive call
+#to findMedianSortedArrays if the length of the nums2 array is less than the 
+#length of the nums1 array. However, testing indicates that this approach has
+#higher run time and greater memory usage than the above approach.
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        totalNumLength = len(nums1) + len(nums2)
+        halfTotalNumLength = totalNumLength // 2
+
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(self, nums2, nums1)
+
+        l, r = 0, len(nums1) - 1
+
+        while True:
+            mid = (l + r) // 2
+            nums1Left = nums1[mid] if mid >= 0 else float('-inf')
+            nums1Right = nums1[mid + 1] if mid + 1 < len(nums1) else float('inf')
+            
+            #Determining the second list's left partition limit
+            targetIndex = halfTotalNumLength - (mid + 1) - 1
+            nums2Left = nums2[targetIndex] if targetIndex >= 0 else float('-inf')
+            nums2Right = nums2[targetIndex + 1] if targetIndex + 1 < len(nums2) else float('inf')
+
+            if nums1Left <= nums2Right and nums2Left <= nums1Right:
+                if totalNumLength % 2 != 0:
+                    return min(nums1Right, nums2Right)
+                return (max(nums1Left, nums2Left) + min(nums1Right, nums2Right)) / 2
+            elif nums1Left > nums2Right:
+                r = mid - 1
+            else:
+                l = mid + 1
---
+++ linked-list/reverse-linked-list.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        curr = head
+        prev = None
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        
+        return prev
