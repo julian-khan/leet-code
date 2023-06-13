300031d44c2993556b7ccba440b66fbe472be425
Julian Khan <juliankhan.b@gmail.com>
2023-06-13 16:16:07 +0800

    Complete Add Two Numbers challenge

---
+++ linked-list/copy-list-with-random-pointer.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/copy-list-with-random-pointer/
+
+#Time complexity O(n), space complexity O(n)
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        origToCopied = {}
+
+        curr = head
+        while curr:
+            origToCopied[curr] = Node(curr.val, None, None)
+            curr = curr.next
+        
+        curr = head
+        while curr:
+            origToCopied[curr].next = origToCopied.get(curr.next, None)
+            origToCopied[curr].random = origToCopied.get(curr.random, None)
+            curr = curr.next
+        
+        return origToCopied.get(head)
