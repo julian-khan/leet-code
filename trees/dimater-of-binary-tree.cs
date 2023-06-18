2e8d7723751fc7ceb2de2789bda2a79f26383e81
Julian Khan <juliankhan.b@gmail.com>
2023-06-18 10:07:44 +0800

    Optimise solution for Diameter of Binary Tree

--- trees/dimater-of-binary-tree.cs
+++ trees/dimater-of-binary-tree.cs
@@ -13,23 +13,36 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 public class Solution {
+    private int result = 0;
+
     public int DiameterOfBinaryTree(TreeNode root) {
         if(root == null) return 0;
 
-        int leftMax = DfsMaxEdgePath(root.left);
-        int rightMax = DfsMaxEdgePath(root.right);
-
-        var recursiveLeft = DiameterOfBinaryTree(root.left);
-        var recursiveRight = DiameterOfBinaryTree(root.right);
-
-        return Math.Max(leftMax + rightMax, Math.Max(recursiveLeft, recursiveRight));
+        DfsMaxEdgePath(root);
+        return result;
     }
 
     private int DfsMaxEdgePath(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) return -1;
 
-        int longestEdge = Math.Max(DfsMaxEdgePath(root.left), DfsMaxEdgePath(root.right));
-        return 1 + longestEdge;
+        int leftMaxEdges = 1 + DfsMaxEdgePath(root.left);
+        int rightMaxEdges = 1 + DfsMaxEdgePath(root.right);
+        result = Math.Max(result, leftMaxEdges + rightMaxEdges);
+
+        return Math.Max(leftMaxEdges, rightMaxEdges);
     }
 }
